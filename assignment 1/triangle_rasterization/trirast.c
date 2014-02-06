840a8b27ca466db1b6204b35809e144b3c62fadb
Tycho Nijon <tychonijon@gmail.com>
2014-02-06 15:05:40 +0100

    shared edges fixed

--- assignment 1/triangle_rasterization/trirast.c
+++ assignment 1/triangle_rasterization/trirast.c
@@ -28,28 +28,37 @@
  */
 
 void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
-    byte r, byte g, byte b) {
+        byte r, byte g, byte b) {
     int xmin, xmax, ymin, ymax;
-    float alpha, beta, gamma;
-    
+    float alpha, beta, gamma, func_alpha, func_beta, func_gamma;
+
     xmin = floor(fmin(x0, fmin(x1, x2)));
     xmax = ceil(fmax(x0, fmax(x1, x2)));
     ymin = floor(fmin(y0, fmin(y1, y2)));
     ymax = ceil(fmax(y0, fmax(y1, y2)));
-    
+
+    func_alpha = func(y1, y2, x2, x1, x0, y0);
+    func_beta = func(y2, y0, x0, x2, x1, y1);
+    func_gamma = func(y0, y1, x1, x0, x2, y2);
+
     for (int y = ymin; y <= ymax; y++) {
         for (int x = xmin; x <= xmax; x++) {
-            alpha = func(y1, y2, x2, x1, x, y) / func(y1, y2, x2, x1, x0, y0);
-            beta = func(y2, y0, x0, x2, x, y) / func(y2, y0, x0, x2, x1, y1);
-            gamma = func(y0, y1, x1, x0, x, y) / func(y0, y1, x1, x0, x2, y2);
-            
-            if (alpha > 0 && beta > 0 && gamma > 0) {
-                PutPixel(x, y, alpha * r, beta * g, gamma * b);
+            alpha = func(y1, y2, x2, x1, x, y) / func_alpha;
+            beta = func(y2, y0, x0, x2, x, y) / func_beta;
+            gamma = func(y0, y1, x1, x0, x, y) / func_gamma;
+
+            if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+                if ((alpha > 0 || func_alpha * func(y1, y2, x2, x1, -1, -1) > 0)
+                        && (beta > 0 || func_beta * func(y2, y0, x0, x2, -1, -1) > 0)
+                        && (gamma > 0 || func_gamma * func(y0, y1, x1, x0, x, y) > 0)
+                        ) {
+                    PutPixel(x, y, alpha * r, beta * g, gamma * b);
+                }
             }
         }
     }
-    
-    
+
+
 }
 
 float func(float a0, float a1, float b0, float b1, float x, float y) {
@@ -57,6 +66,6 @@ float func(float a0, float a1, float b0, float b1, float x, float y) {
 }
 
 void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2, float y2,
-    byte r, byte g, byte b) {
-    
+        byte r, byte g, byte b) {
+
 }
