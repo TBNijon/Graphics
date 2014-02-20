a34ac463c02ad2cb0b50ce9620f5ecf878126a64
Max Grim <max.grim@student.uva.nl>
2014-02-20 12:22:53 +0100

    Removed executables Bezier curves drawed for 2, 3, and 4 control points

--- assignment 3/bezier_curves/bezier.c
+++ assignment 3/bezier_curves/bezier.c
@@ -4,14 +4,11 @@
  * Date ............ 22.07.2009
  * Created by ...... Paul Melis
  *
- * Student name ....
- * Student email ...
- * Collegekaart ....
- * Date ............
- * Comments ........
- *
- *
- * (always fill in these fields before submitting!!)
+ * Student name .... Max Grim & Tycho Nijon
+ * Student email ... max.grim@student.uva.nl & tychonijon@gmail.com
+ * Collegekaart .... 10431365 & 10385762
+ * Date ............ 20-02-2013
+ * Comments ........ None
  */
 
 #include <math.h>
@@ -26,11 +23,17 @@
  * respectively.
  */
 
-void
-evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u)
-{
-    *x = 0.0;
-    *y = 0.0;
+void evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u) {
+    if(num_points == 2) {
+        *x = (1 - u) * p[0].x + u * p[1].x;
+        *y = (1 - u) * p[0].y + u * p[1].y;
+    } else if(num_points == 3) {
+        *x = pow(1 - u, 2) * p[0].x + ((2 * (1 - u)) * u * p[1].x) + (pow(u, 2) * p[2].x);
+        *y = pow(1 - u, 2) * p[0].y + ((2 * (1 - u)) * u * p[1].y) + (pow(u, 2) * p[2].y);
+    } else if(num_points == 4) {
+        *x = pow(1 - u, 3) * p[0].x + 3 * (pow(1 - u, 2) * u * p[1].x) + 3 * ((1 - u) * pow(u, 2) * p[2].x) + pow(u, 3) * p[3].x;
+        *y = pow(1 - u, 3) * p[0].y + 3 * (pow(1 - u, 2) * u * p[1].y) + 3 * ((1 - u) * pow(u, 2) * p[2].y) + pow(u, 3) * p[3].y;
+    }
 }
 
 /* Draw a Bezier curve defined by the control points in p[], which
@@ -54,20 +57,30 @@ evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, flo
  * the curve.
  */
 
-void
-draw_bezier_curve(int num_segments, control_point p[], int num_points)
-{
+void draw_bezier_curve(int num_segments, control_point p[], int num_points) {
+    float x1, y1, x2, y2;
+
+    for(int i = 0; i < num_segments; i++) {
+        float u = i / (float) num_segments;
+        float u2 = (i + 1) / (float) num_segments;
+
+        evaluate_bezier_curve(&x1, &y1, p, num_points, u);
+        evaluate_bezier_curve(&x2, &y2, p, num_points, u2);
+
+        glBegin(GL_LINE_STRIP);
+        glVertex2f(x1, y1);
+        glVertex2f(x2, y2);
+        glEnd();
+    }
 }
 
 /* Find the intersection of a cubic Bezier curve with the line X=x.
    Return 1 if an intersection was found and place the corresponding y
    value in *y.
    Return 0 if no intersection exists.
-*/
+   */
 
-int
-intersect_cubic_bezier_curve(float *y, control_point p[], float x)
-{
+int intersect_cubic_bezier_curve(float *y, control_point p[], float x) {
     return 0;
 }
 
